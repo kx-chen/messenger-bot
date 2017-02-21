74af95694252e15a1c887e82dfc3057d007dca41
System <none>
2017-02-21 00:47:24 +0000

    Next bus feature

--- recievedMessage.js
+++ recievedMessage.js
@@ -16,8 +16,8 @@ var recievedMessage = function(event) {
     var messageId = message.mid;
 
     var messageText = message.text;
-  
-   
+
+
     var messageAttachments = message.attachments;
 
     if (messageText) {
@@ -35,8 +35,8 @@ var recievedMessage = function(event) {
             case 'weather':
                 sendTextMessage(senderID, "Here's the weather: http://www.forecast.io");
                 break;
-          case 'nearby':
-              sendTextMessage(senderID, "Sorry, I can't find nearby busses yet.");
+            case 'nearby':
+                sendTextMessage(senderID, "Sorry, I can't find nearby busses yet.");
 
             default:
                 console.log(messageText);
@@ -48,22 +48,50 @@ var recievedMessage = function(event) {
                     }
                 };
 
+                var Estimates = {
+                    url: ('http://api.translink.ca/rttiapi/v1/stops/' + messageText + '/estimates?apikey=nK9aHp8kThROoJjNpcO3&count=3&timeframe=120'),
+                    headers: {
+                        'accept': 'application/JSON'
+                    }
+                };
+
                 request(options, function(error, response, body) {
                     if (!error && response.statusCode == 200) {
                         var info = JSON.parse(body);
-
-                        console.log(info.Name);
-                        console.log(typeof(body));
+                        
+                        var date = new Date();
+                        var utcDate = new Date(date.toUTCString());
+                        utcDate.setHours(utcDate.getHours()-8);
+                        var usDate = new Date(utcDate);
                       
-                        sendTextMessage(senderID, messageText + " is at " + info.Name + " in " + info.City + ". Buses include " + info.Routes);
+                        var hours = usDate.getHours();
+                        var minutes = usDate.getMinutes();
+
+                      
+                        sendTextMessage(senderID, "The current time is " + hours + ":" + minutes);
+                        sendTextMessage(senderID, messageText + " is at " + info.Name + " in " + info.City + ". Buses include " + info.Routes + ".");
+
+                      
+                        request(Estimates, function(error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                                var info = JSON.parse(body);
+                                
+                                
+                    
+                                var leave1 = info[0].Schedules[0].ExpectedLeaveTime;
+                                var leave2 = info[0].Schedules[1].ExpectedLeaveTime;
+                              
+                                sendTextMessage(senderID, "The next bus for " + messageText + " leaves at " + leave1 + " then at, " + leave2);
+                            }
+                        });
 
                     } else {
                         sendTextMessage(senderID, "Please provide a valid bus stop. Say 'help' for assistance. ")
-                    }
+                      }
                 });
         }
 
-      
+
     } else if (messageAttachments) {
 
         messageAttachments.forEach(function(messageAttachment) {
